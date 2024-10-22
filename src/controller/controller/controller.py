2ff305770f1560ee322f3e870ba8469407d35dc3
Michael Lu <lu.michael@outlook.com>
2024-10-22 16:55:43 +0000

    param example

--- src/controller/controller/controller.py
+++ src/controller/controller/controller.py
@@ -3,6 +3,30 @@ from rclpy.node import Node
 from geometry_msgs.msg import PoseStamped, Twist, Vector3
 import numpy as np
 
+class ControllerNode(Node):
+    def __init__(self) -> None:
+        super().__init__('robot_controller')
+        self.sub = self.create_subscription(PoseStamped, '/robot_pose', self.send_ctrl, 10)
+        self.pub = self.create_publisher(Twist, '/cmd_vel', 1)
+
+        self.declare_parameter('algorithm', 'DQN')
+        algo: str = self.get_parameter('algorithm').get_parameter_value().string_value
+        self.get_logger().info(f"Using algorithm {algo}")
+    
+    def send_ctrl(self, msg: PoseStamped):
+        robot_x = msg.pose.position.x
+        robot_y = msg.pose.position.y
+        _, _, robot_theta = euler_from_quaternion(msg.pose.orientation)
+        self.get_logger().info(f'Robot position: x={robot_x:0.2f} y={robot_y:0.2f} theta={robot_theta:0.2f}')
+
+        # Do research code 
+
+        twist_msg = Twist()
+        twist_msg.linear = Vector3(x=1.0, y=0.0, z=0.0)
+        twist_msg.angular = Vector3(x=0.0, y=0.0, z=0.0)
+
+        self.pub.publish(twist_msg)
+
 def euler_from_quaternion(quaternion):
     """
     Converts quaternion (w in last place) to euler roll, pitch, yaw
@@ -27,25 +51,6 @@ def euler_from_quaternion(quaternion):
 
     return roll, pitch, yaw
 
-class ControllerNode(Node):
-    def __init__(self) -> None:
-        super().__init__('robot_controller')
-        self.sub = self.create_subscription(PoseStamped, '/robot_pose', self.send_ctrl, 10)
-        self.pub = self.create_publisher(Twist, '/cmd_vel', 1)
-    
-    def send_ctrl(self, msg: PoseStamped):
-        robot_x = msg.pose.position.x
-        robot_y = msg.pose.position.y
-        _, _, robot_theta = euler_from_quaternion(msg.pose.orientation)
-        self.get_logger().info(f'Robot position: x={robot_x:0.2f} y={robot_y:0.2f} theta={robot_theta:0.2f}')
-
-        # Do research code 
-
-        twist_msg = Twist()
-        twist_msg.linear = Vector3(x=1.0, y=0.0, z=0.0)
-        twist_msg.angular = Vector3(x=0.0, y=0.0, z=0.0)
-
-        self.pub.publish(twist_msg)
         
 def main(args=None):
     rclpy.init(args=args)
--- src/controller/launch/experiment.launch.py
+++ src/controller/launch/experiment.launch.py
@@ -1,13 +1,18 @@
+import os
+from ament_index_python import get_package_share_directory
 from launch import LaunchDescription
 from launch_ros.actions import Node
 
 def generate_launch_description():
     ld = LaunchDescription()
 
+    config = os.path.join(get_package_share_directory('controller'), 'param', 'robot_params.yaml')
     controller_node = Node(
         package="controller",
         executable="robot_controller",
+        parameters = [config]
     )
+
     sim_node = Node(
         package="vicon_simulator",
         executable="robot_pose_simulator",
