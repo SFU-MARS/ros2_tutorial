e4c30f2199e2a21770ba6e35fa488ff4f761ae7c
Andy <tianyitang_2021@163.com>
2025-03-02 05:16:53 +0000

    creat the models file and simple_robot.urdf.xacro for building robot in gazebo and add the reference in setup.py

--- src/multiple_robots_simulation/setup.py
+++ src/multiple_robots_simulation/setup.py
@@ -14,6 +14,7 @@ setup(
         ('share/' + package_name, ['package.xml']),
         (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')), 
         (os.path.join('share', package_name, 'worlds'), glob('worlds/*.world')),
+        (os.path.join('share', package_name, 'models'), glob('models/*.urdf.xacro')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
