d2c71a4d420d619c214a6f59b525ce02830f1b88
Andy <tianyitang_2021@163.com>
2025-03-05 23:57:52 +0000

    Modify the __init__ function in bvc_controller.py to setup initial information about BVC and robot moving speed

--- src/multiple_robots_simulation/multiple_robots_simulation/bvc_controller.py
+++ src/multiple_robots_simulation/multiple_robots_simulation/bvc_controller.py
@@ -1,9 +1,21 @@
 from rclpy.node import Node
 import rclpy
+import numpy as np
 
 class BVCController(Node):
     def __init__(self):
         super().__init__('bvc_controller')
+
+        self.safety_radius = 0.3
+        self.max_linear_speed = 0.2
+        robot_config = "robot_config.yaml"
+        world_size = 15
+        self.max_angular_speed = 0.5
+
+        self.world_corners = np.array([
+            [-world_size, -world_size, world_size, world_size], 
+            [-world_size, world_size, world_size, -world_size]
+        ])
 
 
 def main(args=None):
