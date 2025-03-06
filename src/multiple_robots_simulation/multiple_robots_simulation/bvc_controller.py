65b81717c927f8191ca5a51c199ef6396ad7dc97
Andy <tianyitang_2021@163.com>
2025-03-06 00:03:11 +0000

    Modify the bvc_controller.py to make it load information by given config

--- src/multiple_robots_simulation/multiple_robots_simulation/bvc_controller.py
+++ src/multiple_robots_simulation/multiple_robots_simulation/bvc_controller.py
@@ -1,6 +1,9 @@
 from rclpy.node import Node
 import rclpy
+import os
 import numpy as np
+import yaml
+from ament_index_python.packages import get_package_share_directory
 
 class BVCController(Node):
     def __init__(self):
@@ -16,6 +19,25 @@ class BVCController(Node):
             [-world_size, -world_size, world_size, world_size], 
             [-world_size, world_size, world_size, -world_size]
         ])
+
+        try:
+            package_dir = get_package_share_directory('multiple_robots_simulation')
+            config_path = os.path.join(package_dir, 'config', robot_config)
+            with open(config_path, 'r') as f:
+                config = yaml.safe_load(f)
+            self.get_logger().info(f'Loaded goals from {config_path}')
+
+            self.robot_count = min(int(config['robot_count']) ,len(config['robot_positions']))
+            self.goals = config['robot_goals']
+                    
+        except Exception as e:
+            self.get_logger().warning(f'Could not load goals config: {e}')
+            # Use default goals if config file is not available
+            self.robot_count = 2
+            self.goals = np.array([
+                [3.0, 3.0],    
+                [-3.0, 3.0]  
+            ])
 
 
 def main(args=None):
