d42aa12451ef06c20aa00e61d354908e1e4a4a44
Andy <tianyitang_2021@163.com>
2025-03-12 02:55:41 +0000

    copy the turtlebot3_state_publisher.launch.py from turtlebot3_bringup into launch folder and set the namespace of robot. Also modify the robot.launch.py to make it reference this file

--- src/multiple_robots_simulation/launch/robot.launch.py
+++ src/multiple_robots_simulation/launch/robot.launch.py
@@ -25,6 +25,7 @@ from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
+from launch.substitutions import ThisLaunchFileDir
 
 
 def generate_launch_description():
@@ -83,7 +84,7 @@ def generate_launch_description():
 
         IncludeLaunchDescription(
             PythonLaunchDescriptionSource(
-                [os.path.join(get_package_share_directory('turtlebot3_bringup'),'launch','/turtlebot3_state_publisher.launch.py')]),
+                [ThisLaunchFileDir(), '/turtlebot3_state_publisher.launch.py']),
             launch_arguments={'use_sim_time': use_sim_time}.items(),
         ),
 
