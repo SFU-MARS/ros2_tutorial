003ef62c4c083abe52cd441e65b715e27adc270a
Andy <tianyitang_2021@163.com>
2025-03-12 02:03:35 +0000

    modify the robot.launch.py to make it correctly change the namespace of robot

--- src/multiple_robots_simulation/launch/robot.launch.py
+++ src/multiple_robots_simulation/launch/robot.launch.py
@@ -95,7 +95,7 @@ def generate_launch_description():
         Node(
             package='turtlebot3_node',
             executable='turtlebot3_ros',
-            namespcae = namespace,
+            node_namespace = namespace,
             parameters=[tb3_param_dir],
             arguments=['-i', usb_port],
             output='screen'),
